112e2f9ad354f62928f7eb2e0751045162750a06
shulya403 <56358220+shulya403@users.noreply.github.com>
2020-05-05 10:53:23 +0300

    Парсинг модификаций на Яндекс Парсит Мвидео отдльным. DNS, Citilink, Eldorado - универсальным. Надо сделать парсинг, Ситилинк, Эльдорадо, ДНС

--- UN_parsing.py
+++ UN_parsing.py
@@ -3,4 +3,5 @@ import parse_universal_classes as pa
 #main
 
 #parse = pa.Parse_CL(category='ноутбук', scraper='requests', num_outfile=3, interrupt=15).Pagination(33, 56)
-parse = pa.Parse_DNS(category='ноутбук', scraper='requests', num_outfile=2, interrupt=1).Pagination(start=2)
+#parse = pa.Parse_DNS(category='ноутбук', scraper='requests', num_outfile=2, interrupt=1).Pagination(start=2)
+parse = pa.Parse_El(category='ноутбук', scraper='requests', num_outfile=1, interrupt=1).Pagination()
