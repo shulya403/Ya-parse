3be94dd85bfcbfd16442214f6afbfb58d12802d1
shulya403 <56358220+shulya403@users.noreply.github.com>
2024-10-21 00:13:39 +0300

    21-10-24

--- UN_parsing.py
+++ UN_parsing.py
@@ -5,7 +5,7 @@ import parse_universal_classes as pa
 #def Pagination(self, start=1, finish=-1)
 
 # Aug-24 parse = pa.Parse_CL(category='ноутбук', scraper='selenium', num_outfile=0, interrupt=15).Pagination() #Следить - все ли (для rquest); selenium подрубается со второго запуска
-parse = pa.Parse_DNS(category='ноутбук', scraper='selenium', num_outfile=1, interrupt=5).Pagination(start=36) #Следить - все ли
+# parse = pa.Parse_DNS(category='ноутбук', scraper='selenium', num_outfile=1, interrupt=5).Pagination(start=36) #Следить - все ли
 #parse = pa.Parse_El(category='ноутбук', scraper='selenium', num_outfile=2 , interrupt=8).Pagination(start=2, finish=101) #Не останавливаться, ставить стопы NB 35 https://www.eldorado.ru/c/noutbuki/
 # Aug-24 parse = pa.Parse_OZ(category='ноутбук', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 # Aug-24 parse = pa.Parse_Ya(category='ноутбук', scraper='selenium', num_outfile=2, interrupt=0, user_id=1).\
@@ -16,7 +16,7 @@ parse = pa.Parse_DNS(category='ноутбук', scraper='selenium', num_outfile=
 #"class": "_24Evj"
 
 # Sep-24 parse = pa.Parse_CL(category='монитор', scraper='selenium', num_outfile=0, interrupt=15).Pagination() #Следить - все ли
-# Sep-24 parse = pa.Parse_DNS(category='монитор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
+# Oct-24 parse = pa.Parse_DNS(category='монитор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 #parse = pa.Parse_El(category='монитор', scraper='selenium', num_outfile=1, interrupt=1).Pagination(finish=26) # мониторы 17 https://www.eldorado.ru/c/monitory/
 # parse = pa.Parse_OZ(category='монитор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 # Aug-24 parse = pa.Parse_Ya(category='монитор', scraper='selenium', num_outfile=0, interrupt=2, user_id=1).\
@@ -30,7 +30,7 @@ parse = pa.Parse_DNS(category='ноутбук', scraper='selenium', num_outfile=
 
 #parse = pa.Parse_Ya(category='проектор', scraper='selenium', num_outfile=0, interrupt=5, user_id=1).\
 #   Pagination(vendors=[])
-# aug-24 parse = pa.Parse_DNS(category='проектор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
+# Oct-24 parse = pa.Parse_DNS(category='проектор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 # Prj ['Acer', 'BenQ', 'Epson', 'Infocus', 'LG', 'NEC', 'Optoma', 'Sony', 'Viewsonic', 'Xiaomi', 'Barco', 'Canon', 'Casio', 'Christie', 'Hiper', 'Hitachi', 'JVC', 'Panasonic', 'Ricoh', 'Smart', 'Vivitek']
 
 # parse = pa.Parse_Ya(category='ибп', scraper='selenium', num_outfile=1, interrupt=1).Pagination(vendors=['APC'])
@@ -38,4 +38,4 @@ parse = pa.Parse_DNS(category='ноутбук', scraper='selenium', num_outfile=
 #             'GE','DKC','Tripp Lite','AEG','Irbis','Sven','Hiper']
 
 #parse = pa.Parse_Ya(category='logitech', scraper='selenium', num_outfile=0, interrupt=1).Pagination(vendors=[])
-#parse = pa.Parse_DNS(category='ибп', scraper='selenium', num_outfile=1, interrupt=5).Pagination()
+# oct-24 parse = pa.Parse_DNS(category='ибп', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
