fe243a5a7c6903a4164df1ff042b4e7b1d9f7464
shulya403 <56358220+shulya403@users.noreply.github.com>
2025-02-19 18:42:41 +0300

    19-02-25

--- UN_parsing.py
+++ UN_parsing.py
@@ -4,8 +4,8 @@ import parse_universal_classes as pa
 
 #def Pagination(self, start=1, finish=-1)
 
-# Jan-25 parse = pa.Parse_CL(category='ноутбук', scraper='selenium', num_outfile=1, interrupt=25).Pagination() #Следить - все ли (для rquest); selenium подрубается со второго запуска
-# jan-25 parse = pa.Parse_DNS(category='ноутбук', scraper='selenium', num_outfile=1, interrupt=5).Pagination(start=15) #Следить - все ли
+# feb-25 parse = pa.Parse_CL(category='ноутбук', scraper='selenium', num_outfile=0, interrupt=10).Pagination() #Следить - все ли (для rquest); selenium подрубается со второго запуска
+# feb-25 parse = pa.Parse_DNS(category='ноутбук', scraper='selenium', num_outfile=3, interrupt=5).Pagination(start=43) #Следить - все ли
 #parse = pa.Parse_El(category='ноутбук', scraper='selenium', num_outfile=2 , interrupt=8).Pagination(start=2, finish=101) #Не останавливаться, ставить стопы NB 35 https://www.eldorado.ru/c/noutbuki/
 #parse = pa.Parse_OZ(category='ноутбук', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 #parse = pa.Parse_Ya(category='ноутбук', scraper='selenium', num_outfile=0, interrupt=0, user_id=1).\
@@ -15,8 +15,8 @@ import parse_universal_classes as pa
 # Nb ["Acer", "Alienware", "Apple", "Acer", "Apple", "Asus", "Dell", "Honor", "HP", "Huawei", "Lenovo", "MSI"]
 #"class": "_24Evj"
 
-# Jan-25 parse = pa.Parse_CL(category='монитор', scraper='selenium', num_outfile=0, interrupt=15).Pagination() #Следить - все ли
-parse = pa.Parse_DNS(category='монитор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
+# feb-25 parse = pa.Parse_CL(category='монитор', scraper='selenium', num_outfile=0, interrupt=15).Pagination() #Следить - все ли
+# feb-25 parse = pa.Parse_DNS(category='монитор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 #parse = pa.Parse_El(category='монитор', scraper='selenium', num_outfile=1, interrupt=1).Pagination(finish=26) # мониторы 17 https://www.eldorado.ru/c/monitory/
 # parse = pa.Parse_OZ(category='монитор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 # Aug-24 parse = pa.Parse_Ya(category='монитор', scraper='selenium', num_outfile=0, interrupt=2, user_id=1).\
@@ -30,7 +30,7 @@ parse = pa.Parse_DNS(category='монитор', scraper='selenium', num_outfile=
 
 #parse = pa.Parse_Ya(category='проектор', scraper='selenium', num_outfile=0, interrupt=5, user_id=1).\
 #   Pagination(vendors=[])
-# Dec-24 parse = pa.Parse_DNS(category='проектор', scraper='selenium', num_outfile=1, interrupt=5).Pagination()
+parse = pa.Parse_DNS(category='проектор', scraper='selenium', num_outfile=0, interrupt=5).Pagination()
 # Prj ['Acer', 'BenQ', 'Epson', 'Infocus', 'LG', 'NEC', 'Optoma', 'Sony', 'Viewsonic', 'Xiaomi', 'Barco', 'Canon', 'Casio', 'Christie', 'Hiper', 'Hitachi', 'JVC', 'Panasonic', 'Ricoh', 'Smart', 'Vivitek']
 
 # parse = pa.Parse_Ya(category='ибп', scraper='selenium', num_outfile=1, interrupt=1).Pagination(vendors=['APC'])
